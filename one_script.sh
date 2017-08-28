b364eb96f900990dc0cf3bc05bd5d1b2f72e2900
Noppadol <noppadol.mututanon@gmail.com>
2017-08-28 14:47:43 +0700

    Remove order , Add Docker

--- one_script.sh
+++ one_script.sh
@@ -17,27 +17,28 @@ output_red () {
     echo $red $word $reset
 }
 
-output_green "### 1.Start Add PPA and do upgrade first###"
+output_green "### One script by Noppadol M. ###"
+output_green "### Start Add PPA and do upgrade first###"
 sudo apt-get update
 sudo add-apt-repository ppa:ondrej/php
 ##sudo apt-get -y upgrade
 
-output_green "### 2.Update ###"
+output_green "### Update ###"
 sudo apt-get update
 
-output_green "### 3.Install zsh ###"
+output_green "### Install zsh ###"
 sudo apt-get -y install terminator
 sudo apt-get -y install zsh
 sudo chsh -s $(which zsh)
-output_green "### 3.1 Install CURL & Oh My Zsh! ###"
+output_green "### Install CURL & Oh My Zsh! ###"
 sudo apt-get -y install curl
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
-output_green "### 4.Install PHP ###"
-sudo apt-get -y install php php-mysql php-soap php-mbstring php-mcrypt
+output_green "### Install PHP ###"
+sudo apt-get -y install php php-mysql php-soap php-mbstring php-mcrypt php-mongodb
 sudo apt-get -y install wget
 
-output_green "### 5 Install Composer ###"
+output_green "### Install Composer ###"
 EXPECTED_SIGNATURE=$(wget -q -O - https://composer.github.io/installer.sig)
 php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
 ACTUAL_SIGNATURE=$(php -r "echo hash_file('SHA384', 'composer-setup.php');")
@@ -53,14 +54,29 @@ php composer-setup.php --quiet
 RESULT=$?
 rm composer-setup.php
 echo $RESULT
-##############
 
-output_green "### 6.Install GIT ###"
+output_green "### Install GIT ###"
 sudo apt-get -y install git
 
-output_green "### 7.Install python & thefuck ###"
+output_green "### Install python ###"
 sudo apt-get -y install python-pip python-dev
+
+output_green "### Install thefuck ###"
 sudo pip install psutil thefuck
 
-output_green "### 8.Install htop ###"
+output_green "### Install htop ###"
 sudo apt-get -y install htop
+
+output_green "### Install Docker ###"
+sudo apt-get -y install \
+    apt-transport-https \
+    ca-certificates \
+    software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo apt-key fingerprint 0EBFCD88
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+sudo apt-get update
+sudo apt-get -y install docker-ce
