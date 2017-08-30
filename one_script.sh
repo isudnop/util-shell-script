0c0af9f5f628ba522aa99613440118849322ef0e
Noppadol <noppadol.mututanon@gmail.com>
2017-08-30 10:44:14 +0700

    :package: Add more stuff

--- one_script.sh
+++ one_script.sh
@@ -17,26 +17,33 @@ output_red () {
     echo $red $word $reset
 }
 
-output_green "### One script by Noppadol M. ###"
+output_green "### One Script Shall Install Them All ###"
 output_green "### Start Add PPA and do upgrade first###"
 sudo apt-get update
-sudo add-apt-repository ppa:ondrej/php
+sudo add-apt-repository ppa:ondrej/php -y
 ##sudo apt-get -y upgrade
 
 output_green "### Update ###"
 sudo apt-get update
 
 output_green "### Install zsh ###"
-sudo apt-get -y install terminator
+sudo apt-get -qy install terminator
 sudo apt-get -y install zsh
 sudo chsh -s $(which zsh)
 output_green "### Install CURL & Oh My Zsh! ###"
-sudo apt-get -y install curl
+sudo apt-get -y install curl wget
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
 output_green "### Install PHP ###"
-sudo apt-get -y install php php-mysql php-soap php-mbstring php-mcrypt php-mongodb
-sudo apt-get -y install wget
+sudo apt-get -y install php \
+    php-mysql \
+    php-soap \
+    php-mbstring \
+    php-mcrypt \
+    php-mongodb \
+    php-cli \
+    php-xml \
+    php-zip \
 
 output_green "### Install Composer ###"
 EXPECTED_SIGNATURE=$(wget -q -O - https://composer.github.io/installer.sig)
@@ -84,3 +91,6 @@ sudo apt-get -y install docker-ce
 output_green "### Install Docker Compose 1.16.0 ###"
 sudo curl -L https://github.com/docker/compose/releases/download/1.16.0-rc1/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
+
+output_green "### Install NodeJs ###"
+sudo apt-get -y install nodejs
